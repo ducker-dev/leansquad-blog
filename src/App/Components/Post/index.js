d472e49d7076126b78eeaa671e4d1d893a56f7f3
Ivan Smirnov <the.ivan.smirnov@gmail.com>
2020-03-19 21:31:21 +0400

    Добавляет функцию изменения поста.

--- src/App/Components/Post/index.js
+++ src/App/Components/Post/index.js
@@ -1,43 +1,137 @@
 import React, {Component} from 'react';
-import "./style.scss";
 import {Link} from "react-router-dom";
+import classNames from "classnames";
+
+import "./style.scss";
 import {dataInteraction} from "../fetchs";
 
 class Post extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.textInputOne = React.createRef();
+    this.textInputTwo = React.createRef();
+    this.state = {
+      change: false,
+      showOption: false
+    }
+  }
+
+  changeForm = e => {
+    e.preventDefault();
+
+    let postTitle = this.textInputOne.current.value;
+    let postBody = this.textInputTwo.current.value;
+
+    dataInteraction(
+      "PUT",
+      {
+        "title": postTitle,
+        "body": postBody
+      },
+      `posts/${this.props.id}`,
+      () => {
+        dataInteraction(
+          "GET",
+          null,
+          "posts",
+          (result) => {
+            this.props.getPosts(result);
+            this.setState({change: false});
+          },
+          (error) => console.log(error)
+        );
+      },
+    );
+  };
+
   render() {
     const {id, title, body, getPosts} = this.props;
 
     return (
       <div className="post">
-        <div className="post__del-post"
-             onClick={() => {
-               dataInteraction(
-                 "DELETE",
-                 null,
-                 `posts/${id}`,
-                 () => {
+        {
+          !this.state.showOption && (
+            <div className="post__show-option"
+                 onClick={() => this.setState({showOption: true})}>
+              ...
+            </div>
+          )
+        }
+        {
+          this.state.showOption && <>
+            <div className={classNames(
+              "post__del-post",
+              {"post__del-post_visible": this.state.change}
+            )}
+                 onClick={() => {
                    dataInteraction(
-                     "GET",
+                     "DELETE",
                      null,
-                     "posts",
-                     (result) => getPosts(result),
+                     `posts/${id}`,
+                     () => {
+                       dataInteraction(
+                         "GET",
+                         null,
+                         "posts",
+                         (result) => getPosts(result),
+                         (error) => console.log(error)
+                       );
+                     },
                      (error) => console.log(error)
                    );
-                 },
-                 (error) => console.log(error)
-               );
-             }}
-        >
-          x
-        </div>
-        <Link to={"/view-post/" + id} className="post__link">
-          <div className="post__title">
-            {title || 'Вы не передали заголовок поста'}
-          </div>
-          <div className="post__body">
-            {body || 'Вы не передали тело поста'}
-          </div>
-        </Link>
+                 }}
+            >
+              <svg width="12" height="15" viewBox="0 0 12 15" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M1.31213 14.2766C1.32906 14.6809 1.66176 15 2.06635 15H9.11356C9.51815 15 9.85085 14.6809 9.86778 14.2766L10.371 3.65259H0.808868L1.31213 14.2766ZM7.18412 6.29141C7.18412 6.12213 7.32138 5.98483 7.49074 5.98483H7.98115C8.1504 5.98483 8.28777 6.12209 8.28777 6.29141V12.3612C8.28777 12.5305 8.15051 12.6678 7.98115 12.6678H7.49074C7.32145 12.6678 7.18412 12.5306 7.18412 12.3612V6.29141ZM5.03816 6.29141C5.03816 6.12213 5.17543 5.98483 5.34475 5.98483H5.83516C6.00441 5.98483 6.14175 6.12209 6.14175 6.29141V12.3612C6.14175 12.5305 6.00452 12.6678 5.83516 12.6678H5.34475C5.17546 12.6678 5.03816 12.5306 5.03816 12.3612V6.29141ZM2.89214 6.29141C2.89214 6.12213 3.0294 5.98483 3.19872 5.98483H3.68917C3.85846 5.98483 3.99576 6.12209 3.99576 6.29141V12.3612C3.99576 12.5305 3.85849 12.6678 3.68917 12.6678H3.19872C3.02944 12.6678 2.89214 12.5306 2.89214 12.3612V6.29141Z"/>
+                <path
+                  d="M10.7061 0.772725H7.455V0.158085C7.455 0.0707985 7.38424 0 7.29691 0H3.8829C3.79562 0 3.72485 0.0707985 3.72485 0.158085V0.772688H0.473704C0.212065 0.772688 0 0.98479 0 1.24643V2.73463H11.1798V1.24647C11.1798 0.984827 10.9677 0.772725 10.7061 0.772725Z"/>
+              </svg>
+            </div>
+            <div className={classNames(
+              "post__change-post",
+              {"post__change-post_visible": this.state.change}
+            )}
+                 onClick={() => this.setState({change: !this.state.change})}
+            >
+              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M3.93517 9.78157L1.39876 7.24575L7.5335 1.11245L10.0699 3.64826L3.93517 9.78157ZM1.16168 7.69244L3.48838 10.0186L0 11.18L1.16168 7.69244ZM10.85 2.87166L10.4101 3.3115L7.87034 0.772334L8.31028 0.332496C8.75344 -0.110832 9.47209 -0.110832 9.91525 0.332496L10.85 1.26707C11.29 1.7116 11.29 2.42726 10.85 2.87166Z"
+                  fill="black"/>
+              </svg>
+            </div>
+          </>
+        }
+        {
+          this.state.change
+            ? <div className="post__wrapper-form">
+              <form className="post__change-form" onSubmit={this.changeForm}>
+                <input className="post__input"
+                       required
+                       type="text"
+                       defaultValue={title}
+                       ref={this.textInputOne}/>
+                <input className="post__input"
+                       required
+                       type="text"
+                       defaultValue={body}
+                       ref={this.textInputTwo}/>
+                <button className="post__button">
+                  Изменить пост
+                </button>
+              </form>
+            </div>
+            : <Link to={"/view-post/" + id} className="post__link">
+              <div className="post__title">
+                {title || 'Вы не передали заголовок поста'}
+              </div>
+              <div className="post__body">
+                {body || 'Вы не передали тело поста'}
+              </div>
+            </Link>
+        }
       </div>
     );
   }
